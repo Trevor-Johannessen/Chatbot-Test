2f7862a18091658791a7f3e127c298f2f3a15db1
Trevor Johannessen <trevor.johannessen@gmail.com>
2025-02-27 23:33:39 +0000

    Added hook for modules to prompt and speak

--- controller.py
+++ controller.py
@@ -14,23 +14,30 @@ class Controller:
         self.listen_duration = config['listen_duration']
         self.ambient_noise_timeout = config['ambient_noise_timeout']
         self.modules = [module.lower() for module in config['modules']]
-        self.context = config['context']
+        self.inital_context = config['context']
         self.mode = config['mode'].lower() if 'mode' in config else "voice"
+        self.config = config
 
         if "default" not in self.modules:
             logger.warning("Warning: 'default' module not included in modules.")
 
-        self.context += "\n\n"
         self.classes = []
         self.interface = Interface(
             log_directory=config['log_directory'],
             names=self.names,
-            context=self.context,
+            context=self.inital_context,
             mode=self.mode,
             context_window=config['context_window'],
             history=config['voice_history_directory'],
             voice_id=config['voice_id']
         )
+        
+        config['interface'] = self.interface
+        config['functions'] = {
+            "prompt": self.prompt,
+            "say": self.say,
+            "get_input": self.get_input
+        }
         for module_name in self.modules:
             module_name_literal = f"modules.{module_name}"
             module = import_module(module_name_literal)
@@ -38,18 +45,27 @@ class Controller:
             for _, cls in classes:
                 if cls.__module__ != module_name_literal:
                     continue
-                self.classes.append(cls(self.interface, config))
-                if hasattr(cls, 'context'):
-                    self.context += f"{cls.context(config)}\n\n"
-        self.interface.initalize_context(self.context)
+                self.classes.append(cls(config))
+        self.refresh_context()
         self.tools = self.__bundle()
+
+    def refresh_context(self):
+        context=f"{self.inital_context}\n\n"
+        for cls in self.classes:
+            if hasattr(cls, 'context'):
+                context += f"{cls.context(self.config)}\n\n"
+        self.interface.refresh_context(context)
+
+    def get_input(self, audio_file=None):
+        return self.interface.get_input(self.listen_duration, self.ambient_noise_timeout, audio_file=audio_file)
 
     def prompt(self, text=None, audio_file=None):
         if not text:
-            text = self.interface.get_input(self.listen_duration, self.ambient_noise_timeout, audio_file=audio_file)
+            text = self.get_input(audio_file=audio_file)
         message = self.interface.parse_text(text)
         if not message:
             return
+        self.refresh_context()
         # Get response
         response = self.interface.prompt(message, tools=self.tools)
         if not response:
@@ -116,7 +132,7 @@ class Controller:
         return self.tools
     
     def __call_tool(self, tool_call):
-        self.interface.clear_recent_context(2)
+        #self.interface.clear_recent_context(1) # raise this to 2 again if bot makes erroneous function calls
         args = json.loads(tool_call.function.arguments)
         for cls in self.classes:
             if hasattr(cls, tool_call.function.name):
