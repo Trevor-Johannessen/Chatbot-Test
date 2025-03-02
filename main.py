63d76e1e8201547f61bf4971cf64bd7cd1593cdb
Trevor Johannessen <trevor.johannessen@gmail.com>
2025-03-02 21:14:06 +0000

    Added option to select custom env file

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ import json
 import os
 import sys
 
-load_dotenv()
 
-config = 'config.json'
-if len(sys.argv) > 1:
-    config = sys.argv[1]
+import argparse
 
-with open(config, 'r') as f:
+parser = argparse.ArgumentParser(description='Process configuration and environment files.')
+parser.add_argument('--config', '-c', default='config.json', help='Path to the configuration file')
+parser.add_argument('--env', '-e', default='.env', help='Path to the environment file')
+args = parser.parse_args()
+
+load_dotenv(args.env)
+
+with open(args.config, 'r') as f:
     config = json.load(f)
     for index, item in enumerate(config['type']):
         if index < len(config['type']) - 1:
