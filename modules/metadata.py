039e5183ac19aaabe84d7149496f006159b144f7
Trevor Johannessen <trevor.johannessen@gmail.com>
2025-03-15 20:07:05 -0400

    Added the date to the metadata context

--- modules/metadata.py
+++ modules/metadata.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Metadata():
     def __init__(self, config):
         pass
@@ -6,4 +8,5 @@ class Metadata():
         additional_context = 'Below are some fun facts you might find useful.\n'
         additional_context += "You are in version 0.0.3.\n"
         additional_context += "You were first created on February 17th, 2025 (You can consider this a birthday).\n"
+        additional_context += f"The date is {datetime.now().strftime('%A, %B %d, %Y')}\n"
         return additional_context
